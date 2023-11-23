9c3cf547a2e8371a5e7bc34d4582f3448760000f
Kiran Kumatkar <139500973+kirankumatkar@users.noreply.github.com>
2023-11-23 13:30:55 +0530

    maven project

---
+++ src/main/java/com/example/check/DemoProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class DemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjectApplication.class, args);
+	}
+
+}
